87cbe629481efc73bde51fcf915a2906453600fd
Nicolas Vargas Ortega <nvortega92@uma.es>
2016-01-18 11:40:26 +0100

    Create Ejer13.s

---
+++ Ejer13.s
@@ -0,0 +1,100 @@
+	.include "inter.inc"
+.text
+	mov    r0, #0
+	ADDEXC 0x18, irq_handler
+	
+	mov	r0, #0b11010010
+	msr	cpsr_c,  r0
+	mov	sp, #0x8000
+	mov	r0, #0b11010011
+	msr	cpsr_c,  r0
+	mov	sp, #0x8000000
+	
+	ldr    r0, =GPBASE
+	/* guia bits   xx999888777666555444333222111000 */
+	ldr    r1, =0b00001000000000000001000000000000
+	str    r1, [r0, #GPFSEL0]   @ Configure GPIO9 y GPIO4 (altavoz)
+	/* guia bits   xx999888777666555444333222111000 */
+	ldr    r1, =0b00000000001000000000000000001001
+	str    r1, [r0, #GPFSEL1]   @ Configure GPIO10 y 11 y 17
+	/* guia bits   xx999888777666555444333222111000 */
+	ldr    r1, =0b00000000001000000000000001000000
+	str    r1, [r0, #GPFSEL2]   @ Configure GPIO22 y 27
+	
+	ldr	r0, =STBASE
+	ldr	r1, [r0, #STCLO]
+	add	r1, #2
+	str	r1, [r0, #STC1]
+	str	r1, [r0, #STC3]
+	
+	ldr	r0, =INTBASE
+	mov	r1, #0b1010
+	str    r1, [r0, #INTENIRQ1]
+	
+	mov    r0, #0b01010011      @ SVC mode, IRQ enabled
+	msr    cpsr_c, r0
+	
+buc:	b      buc
+
+irq_handler:
+	push   {r0, r1, r2, r3}
+	
+	ldr	r0, =STBASE
+	ldr	r1, =GPBASE
+	ldr	r2, [r0, #STCS] @ NPI
+	ands	r2, #0b0010
+	beq 	altavoz
+	
+	ldr r2, =cuenta
+	
+	ldr    r3, =0b00001000010000100000111000000000 @ Hay que usar ldr en vez de mov cuando hay muchos unos
+	str	r3, [r1, #GPCLR0] @ Apagamos todos los leds
+	ldr 	r3, [r2]
+	subs	r3, #1
+	moveq r3, #6
+	str	r3, [r2]
+	ldr 	r3, [r2, +r3, LSL #2]
+	str	r3, [r1, #GPSET0]
+	
+	mov	r3, #0b0010 @ NPI
+	str	r3, [r0, #STCS]
+	
+	ldr	r3, [r0, #STCLO]
+	ldr	r2, =200000
+	add	r3, r2
+	str	r3, [r0, #STC1]
+	
+	ldr	r3, [r0, #STCS]
+	ands	r3, #0b0100
+	beq	final
+	
+altavoz:	 ldr	r2, =bitson
+	ldr	r3, [r2]
+	eors	r3, #1
+	str	r3, [r2]
+	
+	mov	r3, #0b10000
+	streq	r3, [r1, #GPSET0]
+	strne	r3, [r1, #GPCLR0]
+	
+	mov	r3, #0b1000
+	str	r3, [r0, #STCS]
+	
+	ldr	r3, [r0, #STCLO]
+	ldr	r2, =1136
+	add	r3, r2
+	str	r3, [r0, #STC3]
+	
+final:	pop    {r0, r1, r2, r3}
+	subs   pc, lr, #4
+
+bitson:	.word 0
+cuenta: 	.word 1
+
+secuen:	.word	0b1000000000000000000000000000
+		.word	0b0000010000000000000000000000
+		.word	0b0000000000100000000000000000
+		.word	0b0000000000000000100000000000
+		.word	0b0000000000000000010000000000
+		.word	0b0000000000000000001000000000
+	
