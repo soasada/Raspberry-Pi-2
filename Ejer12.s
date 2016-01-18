ebc2a9591a6dcdff8d330aa3df5a2fdc7c914486
Nicolas Vargas Ortega <nvortega92@uma.es>
2016-01-18 11:02:52 +0100

    Update Ejer12.s

--- Ejer12.s
+++ Ejer12.s
@@ -15,12 +15,12 @@
 	str	r1, [r0, #GPSET0]
 	
 	mov 	r1, #0b00000000000000000000000000001100
-	str	r1, [r0, #GPFEN0]
+	str	r1, [r0, #GPFEN0] @ Capturamos interrupciones de GPIO2 y GPIO3 
+	
 	ldr	r0, =INTBASE
-	
 	mov	r1, #0b00000000000100000000000000000000
+	str    r1, [r0, #INTENIRQ2] @ Activamos el bit 20 para poder saltar el obstaculo
 	
-	str    r1, [r0, #INTENIRQ2]
 	mov    r0, #0b01010011      @ SVC mode, IRQ enabled
 	msr    cpsr_c, r0
 	
@@ -33,15 +33,15 @@ irq_handler:
 	ldr    r1, =0b00000000000000000000011000000000 @ Hay que usar ldr en vez de mov cuando hay muchos unos
 	str	r1, [r0, #GPCLR0] @ Apagamos todos los leds
 	
-	ldr	r1, [r0, #GPEDS0]
+	ldr	r1, [r0, #GPEDS0] @ Consultamos si se ha pulsado el botón GPIO2 
 	ands	r1, #0b00000000000000000000000000000100
 	
-	movne	r1, #0b00000000000000000000001000000000
-	moveq	r1, #0b00000000000000000000010000000000
-	str	r1, [r0, #GPSET0]
+	movne	r1, #0b00000000000000000000001000000000 @ (r1 != 0) Sí: Activamos GPIO9
+	moveq	r1, #0b00000000000000000000010000000000 @ (r1 == 0) No: Activamos GPIO10, lo que significa que se ha pulsado el otro botón ya que si no se hubiera pulsado ninguno no hubiera entrado en la interrupcion
+	str	r1, [r0, #GPSET0] @ Encendemos el led que corresponda
 	
 	mov	r1, #0b00000000000000000000000000001100
-	str	r1, [r0, #GPEDS0]
+	str	r1, [r0, #GPEDS0] @
 	
 	pop    {r0, r1}
 	subs   pc, lr, #4
